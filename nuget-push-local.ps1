27555149f822166f703a8625e52241b5ab546384
Mustafa Oral <drumex@gmail.com>
2021-08-01 16:14:03 +0100

    Updated NuGet local script

--- nuget-push-local.ps1
+++ nuget-push-local.ps1
@@ -3,5 +3,5 @@ $versionSuffix = [string]::Format("{0:yyyyMMdd}.{1}", [DateTime]::Now, [Convert]
 dotnet clean --configuration Release
 dotnet pack --configuration Release --verbosity Normal Moss.Extensions\Moss.Extensions.csproj --version-suffix $versionSuffix
 
-$filename = dir .\Moss.Extensions\bin\Release\*$versionSuffix.nupkg | select Name
+$filename = Get-ChildItem .\Moss.Extensions\bin\Release\*$versionSuffix.nupkg | Select-Object Name
 nuget add ".\Moss.Extensions\bin\Release\$($filename.Name)" -s d:\dev\local-nuget-feed
